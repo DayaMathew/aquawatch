1dd2667647e4d41816dfc26c4c94ca9f55938d37
DayaMathew <dayamathew29@gmail.com>
2026-01-04 01:11:38 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@ function checkWater() {
     let area = document.getElementById("area").value;
 
     if (area === "") {
-        document.getElementById("result").innerHTML = "Please enter an area.";
+        document.getElementById("result").innerHTML =
+            "<span style='color:red'>Please enter an area.</span>";
     } else {
         document.getElementById("result").innerHTML =
             "Status for <b>" + area + "</b>: <span style='color:green'>Safe for Drinking</span>";
     }
+}
+
+function showAlert() {
+    document.getElementById("alertBox").innerHTML =
+        "⚠ Alert: Water contamination reported in nearby areas. Please boil water before use.";
 }
 
 function submitReport(event) {
